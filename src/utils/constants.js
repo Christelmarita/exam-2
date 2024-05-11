f307031d35f814dae923b132eb11a889f29cd9ff
Christelmarita <christelmarita.oe@gmail.com>
2024-05-11 15:47:55 +0200

    adds constants

---
+++ src/utils/constants.js
@@ -0,0 +1,12 @@
+export const baseUrl = "https://v2.api.noroff.dev/";
+export const registerUrl = baseUrl + "auth/register";
+export const loginUrl = baseUrl + "auth/login";
+export const venuesUrl = baseUrl + "holidaze/venues";
+export const bookingsUrl = baseUrl + "holidaze/bookings";
+
+
+
+export const queryCustomer = "_customer";
+export const queryVenue = "_venue";
+export const queryOwner = "_owner";
+export const queryBooking = "_bookings";
